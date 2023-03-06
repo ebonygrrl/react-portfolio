85a6bba01e8acb8ff92c9ec064e90623b4a8f5d5
ebonygrrl <abooker77@yahoo.com>
2023-03-05 20:35:49 -0600

    adding portfolio pages

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Nav() {
+
+    return (
+        <footer>
+            <section class="footer-wrap ta-c">Copyright &copy; 2023. Tori Booker. All Rights Reserved.</section>
+        </footer>  
+    );
+}
