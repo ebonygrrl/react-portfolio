f94f3e40ba3cfd5a81346e886ebd4c8d8a562185
ebonygrrl <abooker77@yahoo.com>
2023-03-06 22:17:09 -0600

    imported stylesheet, need to update css and fix bugs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Work from './components/pages/Work'
+import Work from './components/pages/Work';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import './components/styles/style.css'
 
 function App() {
   return (
     <>
     <Header />
+    <About />
     <Work />
+    <Contact />
     <Footer />
     </>
   );
