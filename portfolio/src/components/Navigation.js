9ec91ca0c1511850b5ab84702f8f23fce25e28b7
ebonygrrl <abooker77@yahoo.com>
2023-03-08 13:53:44 -0600

    added resume page, updated navigation

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
-export default function Nav() {
+export default function Nav({ currentPage, handlePageChange }) {
   const navStyle = { display: 'flex', listStyle: 'none', margin: '0', padding: '0' };
 
   return (
     <nav>
         <ul style={navStyle}>
-            <li><a href="#aboutme">About Me</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#resume">Resume</a></li>
+            <li>
+              <a href="#aboutme"
+                 onClick={() => handlePageChange('Home')}
+                 className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>About Me</a>
+            </li>
+            <li>
+              <a href="#work"
+                 onClick={() => handlePageChange('Work')}
+                 className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}>Work</a>
+            </li>
+            <li>
+              <a href="#contact"
+                 onClick={() => handlePageChange('Contact')}
+                 className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
+            </li>
+            <li>
+              <a href="#resume"
+                 onClick={() => handlePageChange('Resume')}
+                 className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
+            </li>
         </ul>
     </nav>
   );
---
+++ portfolio/src/components/pages/Resume.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <section id="aboutme" class="sec-wrap">
+        <header class="section-title">
+            <h2>Resume</h2>
+        </header>
+        <article class="section-content">
+        <h3>Professional Summary</h3>
+
+<p>Experienced email and web designer with an emphasis on optimizing email performance, responsive mobile design, and digital user experience</p>
+
+
+<h3>Career Summary</h3>
+
+<strong>Ebony Media Group</strong> &bull; <em>Content Marketing Manager</em>
+
+<strong>November 2021 - Present</strong>
+
+<ul>
+<li>Oversee email marketing process - including research, content creation, and layout planning.</li>
+<li>Conceptualize email campaigns to increase engagement and meet sponsorship agreements.</li>
+<li>Monitor and report on consumer behavior through establishing benchmarks to amplify the customer experience, setting engagement goals to grow subscribers, and supporting funnel flow between email and social efforts.</li>
+<li>Collaborate cross-functionally with designers, social media managers, and website specialists to develop strategy and seek continual improvement in all digital marketing efforts.</li>
+</ul>
+
+<strong>TCS Education System</strong> &bull; <em>Creative Technologist</em>
+
+<strong>October 2017 - November 2021</strong>
+
+<li>Served as subject matter expert for digital experiences for internal teams and external stakeholders to educate and provide thought leadership on interactive technologies.</li>
+<li>Managed the infrastructure, analytics, targeting, testing, and tooling requirements for websites and digital platforms.</li>
+<li>Developed and executed solutions to enhance the digital customer journey and user experience for several higher education entities.</li>
+<li>Performed website updates that align with brand enhancement objectives and compliance standards.</li>
+
+<strong>Sears Holdings</strong> &bull; <em>Email Developer</em>
+
+<strong>August 2013 - June 2017</strong>
+
+<li>Responsible for quick and efficient, error-free coding mobile-responsive promotional, triggered, and transactional email campaigns for Sears, Kmart, Craftsman, and Shop Your Way.</li>
+<li>Utilized HTML nested table structure for emails, responsive CSS for mobile. </li>
+<li>Prepared email assets from Adobe Photoshop templates.</li>
+
+<strong>OpticsPlanet, Inc.</strong> &bull; <em>Web Designer</em>
+
+<strong>October 2009 - August 2013</strong>
+
+<li>Created websites using HTML, CSS, and customized WordPress plugins.</li>
+<li>Performed company blog migration from Blogger to Wordpress.</li>
+<li>Installed, configured and managed WordPress installations.</li>
+<li>Specialized in email deliverability, email standards, and creating multipart HTML emails that render consistently for Outlook, Yahoo, Gmail, Hotmail, AOL and devices such as iOS and Android.</li>
+<li>Developed Facebook Page Tab Apps.</li>
+
+<h3>Proficiencies</h3>
+
+Email Development &bull; Responsive Web Design &bull; Wordpress &bull; Copyediting &bull; HTML5 &bull;
+CSS3 &bull; Content Management &bull; Site Maintenance &bull; Quality Control (QA)
+
+<h3>Programming</h3>
+
+Node JS &bull; React &bull; ExpressJS &bull; MongoDB &bull; MySQL &bull; HTML5 &bull; CSS3 &bull; JQuery &bull; Javascript
+
+<h3>Tools</h3>
+Adobe Photoshop CS6 &bull; Adobe Dreamweaver CS6 &bull; Microsoft Outlook &bull; Microsoft Excel
+        </article>
+    </section>
+  );
+}
