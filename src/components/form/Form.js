0d66aac8446529ec244a25263e29a2ca568ce4c7
ebonygrrl <abooker77@yahoo.com>
2023-03-13 01:28:36 -0500

    fixing form validation

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,13 +5,16 @@ import { Form } from 'react-bootstrap';
 import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
-  // set variables for form fields using `useState`
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [nameError, setNameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [messageError, setMessageError] = useState('');
+  // useState object for storing
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  // capture input onchange
+  const setField = (name, value) => {
+    setForm({ ...form, [name]:value });
+
+    !!errors[name] ? setErrors({ ...errors, [name]:null }) : {};
+  }
 
   // validate form entries
   const [validated, setValidated] = useState(false);  
@@ -20,21 +23,19 @@ function Contact() {
   const handleInputChange = (e) => {
     
     // get value and name of input triggered
-    const { name, value } = e.target;
+    //const { name, value } = e.target;
 
-    if (name !== e.currentTarget) {
-      if (fullName.length < 2) {
-        setNameError('Please enter your full name.');
-      }
-      
-      if (!validateEmail(email)) {
-        setEmailError('Please enter a valid email.');
-      } 
+    // if (setFullName(value) === '' || fullName.length < 2) {
+    //   setNameError('Please enter your full name.');
+    // }
+    
+    // if (!validateEmail(email)) {
+    //   setEmailError('Please enter a valid email.');
+    // } 
 
-      if (!setMessage(e.currentTarget.value)) {
-        setMessageError('Please enter a message.');
-      }
-    }
+    // if (setMessage(value) === '') {
+    //   setMessageError('Please enter a message.');
+    // }
   };
 
   const handleFormSubmit = (e) => {
@@ -43,36 +44,59 @@ function Contact() {
     //clear the inputs
     setFullName('');
     setEmail('');
-    setMessage('');    
-    
-    if (!validateEmail(email)) {
-      setEmailError('Please enter a valid email.');
-    } 
-    
-    // alert(`Thank you, ${fullName}! Your message has been sent.`);      
+    setMessage('');      
 
     if (e.currentTarget.checkValidity() === false) {
       e.preventDefault();
       e.stopPropagation();
     }
 
-    setValidated(true);
+    setValidated(true);  
+    
+    // alert(`Thank you, ${fullName}! Your message has been sent.`); 
   };
 
   return (
     <Form noValidate validated={validated}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" name="fullName" value={fullName} onChange={handleInputChange} onBlur={handleInputChange} placeholder="Full Name" required />
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Control 
+          type="text" 
+          name="fullName" 
+          value={fullName} 
+          onChange={handleInputChange} 
+          onBlur={handleInputChange} 
+          placeholder="Full Name" 
+          required 
+        />
         <Form.Control.Feedback type="invalid">{nameError}</Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" name="email" value={email} onChange={handleInputChange} onBlur={handleInputChange} placeholder="Enter email" required />
+        <Form.Control 
+          type="email" 
+          name="email" 
+          value={email} 
+          onChange={handleInputChange} 
+          onBlur={handleInputChange} 
+          placeholder="Enter email" 
+          required 
+        />
         <Form.Control.Feedback type="invalid">{emailError}</Form.Control.Feedback>
       </Form.Group>
 
-      <Form.Control className="mb-3" as="textarea" value={message} onChange={handleInputChange} onBlur={handleInputChange} placeholder="Leave a comment here." required />
+      <Form.Group className="mb-3" controlId="formBasicTextarea">
+      <Form.Control 
+        as="textarea"
+        name="message"
+        rows={3} 
+        value={message} 
+        onChange={handleInputChange} 
+        onBlur={handleInputChange} 
+        placeholder="Leave a comment here." 
+        required 
+      />
       <Form.Control.Feedback type="invalid">{messageError}</Form.Control.Feedback>
+      </Form.Group>
 
       <div className="text-center mt-3">
         <button onClick={handleFormSubmit}>
